1b90b5bdeb83ef7db02b1d4be66d4482e1842be7
quanghuybrightstar <88926687+quanghuybrightstar@users.noreply.github.com>
2021-11-02 22:04:27 +0700

    update

--- workspace/src/ver1/Main.java
+++
@@ -1,8 +0,0 @@
-package ver1;
-
-public class Main {
-    public static void main(String[] args) {
-        DictionaryCommandline dictionaryCommandline = new DictionaryCommandline();
-        dictionaryCommandline.dictionaryBasic();
-    }
-}
